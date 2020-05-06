eee9f42219d3c94bbc5f262a23815356223f22ba
Robin Stewart <robinste@cisco.com>
2020-05-05 17:09:35 -0700

    ng buil set up

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -21,8 +21,8 @@ export class HomeComponent implements OnInit {
     this.displayToDos();
   }
 
-  addToDo() {
-    this.toDoService.addToDos([new Todo(this.toDoText)]).subscribe(res => {
+  addToDo(toDoInfo: string) {
+    this.toDoService.addToDos([new Todo(toDoInfo)]).subscribe(res => {
       this.toDos = this.toDos.concat(res.records);
       this.toDoText = '';
     });
@@ -41,10 +41,16 @@ export class HomeComponent implements OnInit {
     this.toDoService.updateToDos([toDo]).subscribe(res => {});
   }
 
-  updateIsCompleted(toDo: Todo, event: Event) {
+  updateIsCompleted(toDo: Todo, event?: Event) {
     toDo.fields.isCompleted = !toDo.fields.isCompleted;
-    event.stopPropagation();
+    toDo.fields.isStarred = false;
+    if (event) { event.stopPropagation(); }
 
     this.toDoService.updateToDos([toDo]).subscribe(res => {});
+  }
+
+  duplicateToDo(toDo: Todo) {
+    this.addToDo(toDo.fields.Title);
+    this.updateIsCompleted(toDo);
   }
 }
